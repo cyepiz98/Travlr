315e4b169b027851661784782f47b1a03a0cba31
Caesar Yepiz <caesar.yepiz@snhu.edu>
2026-07-19 16:32:01 -0700

    Module 3 complete

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,14 @@
+var fs = require('fs');
+
+console.log("Loading JSON from:", __dirname);
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+console.log("Trips loaded:", trips);
+
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travel' });
+    res.render('travel', { 
+        title: 'Travlr Getaways',
+        trips
+    });
 };
 
 module.exports = {
